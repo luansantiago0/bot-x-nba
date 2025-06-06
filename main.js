4eb470c407a43228064eaa7bf51dfb9a1f194958
luansantiago0 <luansantiago0200@gmail.com>
2025-06-06 10:18:09 -0300

    remove: retirando arquivos que fazem alusão a NBA

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const { iniciarAgendamentos } = require('./src/jobs/scheduler.js');
-// const { buscarProximoJogoNBA } = require('./src/services/nbaService.js');
 const { buscarNoticiasEsportivas } = require('./src/services/newsService.js');
 const { postarNoX } = require('./src/services/twitterService.js');
 
 async function main() {
   console.log('🚀 Iniciando bot...');
-  
-  // const noticiaJogo = await buscarProximoJogoNBA();
-  // if (noticiaJogo) await postarNoX(noticiaJogo);
-
   const noticiaEsportiva = await buscarNoticiasEsportivas();
   if (noticiaEsportiva) await postarNoX(noticiaEsportiva);
 
--- src/jobs/scheduler.js
+++ src/jobs/scheduler.js
@@ -1,15 +1,8 @@
 const cron = require('node-cron');
-// const { buscarProximoJogoNBA } = require('../services/nbaService');
 const { buscarNoticiasEsportivas } = require('../services/newsService');
 const { postarNoX } = require('../services/twitterService');
 
 function iniciarAgendamentos() {
-  // cron.schedule('0 * * * *', async () => {
-  //   console.log('⏰ Postando jogo da NBA...');
-  //   const noticiaJogo = await buscarProximoJogoNBA();
-  //   if (noticiaJogo) await postarNoX(noticiaJogo);
-  // });
-
   cron.schedule('30 * * * *', async () => {
     console.log('⏰ Postando outra notícia esportiva...');
     const noticiaEsportiva = await buscarNoticiasEsportivas();
